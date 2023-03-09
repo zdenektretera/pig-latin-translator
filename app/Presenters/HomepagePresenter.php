4b9678d5d36c575f314142f262a3602e986f4436
ZdenekT <zt@mcrai.eu>
2023-03-10 00:34:24 +0100

    add component to Homepage presenter

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Controls\TranslateControl\TranslateControl;
+use App\Controls\TranslateControl\TranslateControlFactory;
 use Nette;
-
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    public function __construct(
+        private TranslateControlFactory $translateControlFactory
+    ) {
+        parent::__construct();
+    }
+
+    public function createComponentTranslateForm(): TranslateControl
+    {
+        return $this->translateControlFactory->create();
+    }
 }
